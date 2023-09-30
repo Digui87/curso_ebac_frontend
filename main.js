c3e7f740d8c7a4dc64b348d87f9817f2a45c9579
Diagnas Salvador <139275288+Digui87@users.noreply.github.com>
2023-09-30 20:31:49 -0300

    adicionando classe de herança

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
-function Celular(fabricante, modelo, memoria, camera) {
-    this.fabricante = fabricante;
+function Computador(processador, modelo, memoriaRAM, placaDeVideo) {
+    this.processador = processador;
     this.modelo = modelo;
-    this.memoria = memoria;
-    this.camera = camera;
+    this.memoriaRAM = memoriaRAM;
+    this.placaDeVideo = placaDeVideo;
 }
 
-const celular1 = new Celular("Samsung", "S23", "128GB", "50MP");
-const celular2 = new Celular("Apple", "iPhone 14", "128GB", "12MP");
-const celular3 = new Celular("Xiaomi", "12T", "256GB", "108MP");
+const computador1 = new Computador("Intel", "Core i9-13900K", "64GB DDR5", "RTX 4090 24GB");
+const computador2 = new Computador("AMD", "Ryzen 9 5900X", "32GB DDR5", "RTX 4090");
+const computador3 = new Computador("AMD", "Ryzen 5 5600G", "8GB DDR4", "Integrada ao processador");
 
-console.log(celular1);
-console.log(celular2);
-console.log(celular3);
+console.log(computador1);
+console.log(computador2);
+console.log(computador3);
+
+class Notebook extends Computador {
+    constructor(processador, modelo, memoriaRAM, placaDeVideo, tamanhoDaTela) {
+        super(processador, modelo, memoriaRAM, placaDeVideo);
+        this.tamanhoDaTela = tamanhoDaTela;
+    }
+}
+
+const notebook1 = ("Intel", "Core i7 11370H", "16GB DDR4", "GTX 1650 4GB", 15.6);
+const notebook2 = ("Intel", "Core i5 13450HX", "8GB DDR5", "RTX 3050 6GB", 15.6);
+const notebook3 = ("Intel", "Core i7 11800H", "16GB DDR4", "RTX 3070 8GB", 16);
+
+console.log(notebook1);
+console.log(notebook2);
+console.log(notebook3);
+
+class Tablet extends Computador {
+    constructor(processador, modelo, memoriaRAM, placaDeVideo, camera) {
+        super(processador, modelo, memoriaRAM, placaDeVideo);
+        this.camera = camera;
+    }
+}
+
+const tablet1 = ("Snapdragon", "8 Pus Gen 2", "8GB", "Adreno 740", "13MP");
+const tablet2 = ("Apple", "M1", "8GB", "Apple GPU", "12MP");
+const tablet3 = ("Cortex", "A55", "6GB", "Mali-G57", "8MP");
+
+console.log(tablet1);
+console.log(tablet2);
+console.log(tablet3);
 
 const nome = "Diagnas"
 const idade = 36
@@ -54,13 +84,13 @@ function exibeAtributo(nomeAtributo) {
 }
 
 exibeAtributo('nome');
-pessoa['celular'] = celular1;
+pessoa['computador'] = computador1;
 
 exibeAtributo('nome');
-pessoa2['celular'] = celular3;
+pessoa2['computador'] = computador2;
 
 exibeAtributo('nome');
-pessoa3['celular'] = celular2;
+pessoa3['computador'] = computador3;
 
 console.log(pessoa);
 console.log(pessoa2);
